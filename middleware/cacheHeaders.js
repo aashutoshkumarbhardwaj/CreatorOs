7191f3adf41ab8cb57445b9e818af2377aa3ba7f
ionfwsrijan <ionfwsrijan@users.noreply.github.com>
2026-07-08 13:50:44 +0530

    fix: replace broken monkey-patch cache middleware with Express 5-compatible approach (#389)

--- middleware/cacheHeaders.js
+++ middleware/cacheHeaders.js
@@ -1,26 +1,6 @@
-function cacheHeadersMiddleware(req, res, next) {
-    const originalJson = res.json;
-    const originalRender = res.render;
-
-    res.setCacheStatus = function(status) {
-        res.setHeader('X-Cache', status);
-    };
-
-    res.json = function(body) {
-        if (!res.headersSent && !res.hasHeader('X-Cache')) {
-            res.setHeader('X-Cache', 'MISS');
-        }
-        return originalJson.call(this, body);
-    };
-
-    res.render = function(view, locals, callback) {
-        if (!res.headersSent && !res.hasHeader('X-Cache')) {
-            res.setHeader('X-Cache', 'MISS');
-        }
-        return originalRender.call(this, view, locals, callback);
-    };
-
+const cacheHeadersMiddleware = (req, res, next) => {
+    res.setHeader('X-Cache', 'MISS');
     next();
-}
+};
 
 module.exports = cacheHeadersMiddleware;
