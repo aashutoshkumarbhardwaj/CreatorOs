879216178213c4f51a2d5221733ccc15ada6e71f
jasonjose007 <jasonjose006@gmail.com>
2026-09-23 14:49:47 +0530

    fix(ai): replace wrong crypto.v4 import and weak Math.random message IDs
    Fixes #1300

--- controller/aiAssistantController.js
+++ controller/aiAssistantController.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("crypto");
+const { randomUUID } = require("crypto");
 const asyncHandler = require("../utils/asyncHandler");
 const services = require("../services.config");
 const AssistantChat = require("../model/assistantChat");
@@ -68,7 +68,7 @@ const sendMessage = asyncHandler(async (req, res) => {
   }
 
   // User message
-  const userMessageId = `msg_${Date.now()}_${Math.random().toString(36).substr(2, 4)}`;
+  const userMessageId = randomUUID();
   chatDoc.messages.push({
     messageId: userMessageId,
     role: "user",
@@ -86,7 +86,7 @@ const sendMessage = asyncHandler(async (req, res) => {
   });
 
   // Assistant message
-  const assistantMessageId = `msg_${Date.now()}_${Math.random().toString(36).substr(2, 4)}`;
+  const assistantMessageId = randomUUID();
   const assistantMsg = {
     messageId: assistantMessageId,
     role: "assistant",
