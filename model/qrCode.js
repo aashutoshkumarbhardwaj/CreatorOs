304db93cc20c8df683e8b4d7d767d396c09a5b5a
jasonjose007 <jasonjose006@gmail.com>
2026-09-23 17:56:04 +0530

    fix(model): mark QrCode.userId as immutable
    Fixes #1316

--- model/qrCode.js
+++ model/qrCode.js
@@ -10,6 +10,7 @@ const qrCodeSchema = new mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             required: true,
+            immutable: true,
             index: true,
         },
         label: {
